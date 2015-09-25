d2e7e285fe05085312de323282226c70ecebf704
Graphic <graphic@SOD-SJH-516-08M.local>
2015-09-25 14:56:07 -0400

    up to question 4

--- js/main.js
+++ js/main.js
@@ -8,11 +8,12 @@ $('.close').on('click', function(e){
 
 $('.submit').on('click', function(e){
 	$('input').addClass('error');
-	
 });
 
 $('input').on('click', function(e){
 	$(this).removeClass('error');
 });
 
-$('modal').
+$('modal').on('click', function(e){
+	$(this).remove
+});
